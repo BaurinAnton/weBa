f45710d733429a33e58061126e70d3eb13690ae1
BaurinAnton <44928991+BaurinAnton@users.noreply.github.com>
2022-02-13 23:50:10 +0300

    1. Цвета плейсхолдера, неактивной кнопки изменены 2. Добавлена фавиконка 3. Добавлена Яндекс метрика 4. Удален сайтмап

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,23 @@ class MyDocument extends Document {
     return (
       <Html lang="ru">
         <Head>
+          <script dangerouslySetInnerHTML={{
+            __html: `
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(87497757, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   })`}}></script>
+          <noscript dangerouslySetInnerHTML={{
+            __html: `<div><img src="https://mc.yandex.ru/watch/87497757" style="position:absolute; left:-9999px;" alt="" /></div>`
+          }}>
+          </noscript>
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
           <link
             rel="preload"
             href="/fonts/NAMU-Pro.ttf"
